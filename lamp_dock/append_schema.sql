335035b575a4b6fa32b90e4e13316482ed9b3404
hiroshi-38k <mhiroshi92.sora@gmail.com>
2020-07-07 20:50:16 +0900

    テーブル設計の修正（再）

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE histories (
 CREATE TABLE details (
     detail_id INT(11) AUTO_INCREMENT NOT NULL,
     history_id INT(11) NOT NULL,
-    item_id INT(11) NOT NULL,
+    price INT(11) NOT NULL,
+    amount INT(11) NOT NULL,
     primary key(detail_id)
 );
